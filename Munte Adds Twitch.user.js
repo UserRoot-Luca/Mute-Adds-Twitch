576f110fbfb735f6752154a8584114c5c1966d1c
UserRoot-Luca <55756898+UserRoot-Luca@users.noreply.github.com>
2022-10-06 22:25:28 +0200

    Create Munte Adds Twitch.user.js

---
+++ Munte Adds Twitch.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Mute Adds Twitch
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  ###
+// @author       UserRoot-Luca
+// @match        https://www.twitch.tv/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
+// @grant        none
+// ==/UserScript==
+
+(function(){
+    let button_status = true;
+    setInterval(function(){
+        if (document.querySelector('[data-a-target="video-ad-countdown"]') && button_status) {
+            button_status = false;
+            document.querySelector('[data-a-target="player-mute-unmute-button"]').click();
+            console.log("Audio off");
+        } else if(!document.querySelector('[data-a-target="video-ad-countdown"]') && !button_status){
+            button_status = true;
+            document.querySelector('[data-a-target="player-mute-unmute-button"]').click();
+            console.log("Audio on");
+        }
+    }, 300);
+})()
