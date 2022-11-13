efa9e413526417b42ccc8d431b2d39c40ab1c3be
UserRoot-Luca <55756898+UserRoot-Luca@users.noreply.github.com>
2022-11-13 13:03:16 +0100

    Add files via upload

--- Munte Adds Twitch.user.js
+++ Munte Adds Twitch.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Mute Adds Twitch
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.twitch.tv/*
@@ -11,9 +11,9 @@
 // ==/UserScript==
 (function () {
     let ButtonStatus = true;
-    let ButtonAudioElement = document.querySelector('[data-a-target="player-mute-unmute-button"]');
-    let VideoAdElement = document.querySelector('[data-a-target="video-ad-countdown"]');
     setInterval(() => {
+        let ButtonAudioElement = document.querySelector('[data-a-target="player-mute-unmute-button"]');
+        let VideoAdElement = document.querySelector('[data-a-target="video-ad-countdown"]');
         if (VideoAdElement && ButtonStatus) {
             ButtonStatus = false;
             ButtonAudioElement.click();
--- Munte Adds Twitch.user.ts
+++ Munte Adds Twitch.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mute Adds Twitch
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.twitch.tv/*
@@ -9,11 +9,10 @@
 // @grant        none
 // ==/UserScript==
 (function(){
-    let ButtonStatus = true;
-    let ButtonAudioElement = document.querySelector<HTMLButtonElement>('[data-a-target="player-mute-unmute-button"]');
-    let VideoAdElement = document.querySelector<HTMLDivElement>('[data-a-target="video-ad-countdown"]');
-    
+    let ButtonStatus = true;    
     setInterval(()=>{
+        let ButtonAudioElement = document.querySelector<HTMLButtonElement>('[data-a-target="player-mute-unmute-button"]');
+        let VideoAdElement = document.querySelector<HTMLDivElement>('[data-a-target="video-ad-countdown"]');
         if (VideoAdElement && ButtonStatus) {
             ButtonStatus = false;
             ButtonAudioElement!.click();
